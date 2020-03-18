925bc6a0d719b208c8355e3d8448b319e69d4061
ritamonica <59591951+ritamonica@users.noreply.github.com>
2020-03-18 13:15:20 +0700

    Rita
    Donasi

---
+++ application/modules/donasi/controllers/donasi.php
@@ -0,0 +1,56 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class donasi extends MX_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        // model
+        $this->load->model('m_donasi');
+        $this->load->model('login/m_session');
+    }
+
+
+    // index
+    function index()
+    {
+        $data = array(
+            'namamodule'    => "donasi",
+            'namafileview'  => "v_donasi",
+            'tampil'        => $this->m_donasi->tampil(),
+        );
+        echo Modules::run('template/tampilCore', $data);
+    }
+
+    function tambah()
+    {
+        $this->M_donasi->tambah();
+        redirect('donasi');
+    }
+}
+
+    function edit()
+    {
+        $this->M_donasi->edit();
+        redirect('donasi');
+    }
+
+    function hapus($id)
+    {
+        $this->M_donasi->hapus($id);
+        redirect('donasi');
+    }
+
+    function cari()
+    {
+        $data = array(
+            'namamodule'    => "donasi",
+            'namafileview'  => "V_donasi",
+            'tampil'        => $this->M_donasi->cari(),
+        );
+        echo Modules::run('template/tampilCore', $data);
+    }
+
+?>
