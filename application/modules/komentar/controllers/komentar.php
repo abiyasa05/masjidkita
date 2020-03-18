06817a42b10f55c3359a7be3937af11deed3fcb1
ritamonica <59591951+ritamonica@users.noreply.github.com>
2020-03-18 13:22:56 +0700

    Rita
    Komentar

---
+++ application/modules/komentar/controllers/komentar.php
@@ -0,0 +1,56 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class komentar extends MX_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        // model
+        $this->load->model('M_komentar');
+        $this->load->model('login/m_session');
+    }
+
+
+    // index
+    function index()
+    {
+        $data = array(
+            'namamodule'    => "komentar",
+            'namafileview'  => "V_komentar",
+            'tampil'        => $this->M_komentar->tampil(),
+        );
+        echo Modules::run('template/tampilCore', $data);
+    }
+
+    function tambah()
+    {
+        $this->M_komentar->tambah();
+        redirect('komentar');
+    }
+}
+
+    function edit()
+    {
+        $this->M_komentar->edit();
+        redirect('komentar');
+    }
+
+    function hapus($id)
+    {
+        $this->M_komentar->hapus($id);
+        redirect('komentar');
+    }
+
+    function cari()
+    {
+        $data = array(
+            'namamodule'    => "komentar",
+            'namafileview'  => "V_komentar",
+            'tampil'        => $this->M_komentar->cari(),
+        );
+        echo Modules::run('template/tampilCore', $data);
+    }
+
+?>
